0388a89d6e9123c57cff269732b1114180339b0c
Daniel Hengeveld <danielwh@gmail.com>
2017-08-20 20:40:05 +0200

    this isn't async anymore

--- App.js
+++ App.js
@@ -21,7 +21,7 @@ export default class App extends React.Component {
     this.askForPermission()
   }
 
-  async onPress () {
+  onPress () {
     if (this.state.recording) {
       this.stop()
     } else {
